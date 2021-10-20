8b11729e50307fb0922db0284805b3da75eecf00
cosmicray001 <samiulislambracu@gmail.com>
2021-10-20 10:11:08 +0600

    bitSieve add

--- problem_solving/cpp/prime_check/main.cpp
+++ problem_solving/cpp/prime_check/main.cpp
@@ -1,1 +1,15 @@
-
+#include <bits/stdc++.h>
+#define le 100000008
+using namespace std;
+int n[le >> 6];
+#define ck(ve) (n[ve >> 6] & (1 << ((ve >> 1) & 31)))
+#define st(ve) (n[ve >> 6] |= (1 << ((ve >> 1) & 31)))
+void se(){
+    int rt = sqrt(le) + 1, k;
+    for(int i = 3; i < rt; i += 2) if(!ck(i)) for(int j = i * i, k = i << 1; j < le; j += k) st(j);
+}
+int main(){
+    se();
+    for(int i = 3; i < 33; i += 2) if(!ck(i)) printf("%d\n", i);
+    return 0;
+}
